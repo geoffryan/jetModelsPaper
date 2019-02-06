fccd18636f394b7ef46714097790563df659cbf8
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2019-02-06 00:44:55 -0500

    Started with some references, refurbished structure comparison plot.

--- code/structComp.py
+++ code/structComp.py
@@ -48,13 +48,15 @@ def powerlaw(theta, thC=0.1, thW=0.4, E0=3.0e53, n0=1.0, t0=1.0e7, b=3):
         E[offaxis] = 0.0
         g[offaxis] = 1.0
 
-    E[g < 1.0] = 0.0
+    # E[g < 1.0] = 0.0
     g[g < 1.0] = 1.0
 
     return g, E
 
 
 def boostedFireball(theta, gb=3.0, eta0=3.0, E0=1.0e53):
+
+    # returns dE/dOmega scaled to Eiso(0) = E0
 
     ub = math.sqrt(1.0 + gb*gb)
     vb = ub/gb
@@ -120,10 +122,16 @@ def loadTxt(filename, cols, skiprows, delimiter=None):
 
 def duffell2015Oblate(theta, Etot=1.0e49):
 
+    # data is dE/dOmega, scaled as Etot/M_0 c^2 = 0.024
+
     filename = "StructureData/duffell2015_data.csv"
 
     thg, gp = loadTxt(filename, cols=[6, 7], skiprows=2, delimiter=',')
     thE, Ep = loadTxt(filename, cols=[4, 5], skiprows=2, delimiter=',')
+
+    # Msun = 1.989e33
+    # c = 2.99792458e10
+    # M0 = 1.0e-4 * Msun
     Ep *= Etot/0.024
 
     g = np.interp(theta, thg, gp, right=1.0)
@@ -133,6 +141,8 @@ def duffell2015Oblate(theta, Etot=1.0e49):
 
 
 def duffell2015Spherical(theta, Etot=1.0e49):
+
+    # data is dE/dOmega
 
     filename = "StructureData/duffell2015_data.csv"
 
@@ -148,6 +158,8 @@ def duffell2015Spherical(theta, Etot=1.0e49):
 
 def morsony2007realg5(theta, Etot=3.0e51):
 
+    # data is E_iso, total energy is 2.66e52 erg
+
     filename = "StructureData/morsony2007realg5E.csv"
     thp, Ep = np.loadtxt(filename, skiprows=2, unpack=True, delimiter=',')
 
@@ -161,6 +173,8 @@ def morsony2007realg5(theta, Etot=3.0e51):
 
 
 def morsony2007realg2(theta, Etot=3.0e51):
+
+    # data is E_iso, total energy is 2.66e52 erg
 
     filename = "StructureData/morsony2007realg2E.csv"
     thp, Ep = np.loadtxt(filename, skiprows=2, unpack=True, delimiter=',')
@@ -176,6 +190,8 @@ def morsony2007realg2(theta, Etot=3.0e51):
 
 def morsony2007powg5(theta, Etot=3.0e51):
 
+    # data is E_iso, total energy is 2.66e52 erg
+
     filename = "StructureData/morsony2007powg5E.csv"
     thp, Ep = np.loadtxt(filename, skiprows=2, unpack=True, delimiter=',')
 
@@ -189,6 +205,8 @@ def morsony2007powg5(theta, Etot=3.0e51):
 
 
 def morsony2007powg2(theta, Etot=3.0e53):
+
+    # data is E_iso, total energy is 2.66e52 erg
 
     filename = "StructureData/morsony2007powg2E.csv"
     thp, Ep = np.loadtxt(filename, skiprows=2, unpack=True, delimiter=',')
@@ -217,6 +235,8 @@ def kathirgamaraju2017(theta):
 
 def margutti2018(theta):
 
+    # data is dE/dOmega
+
     filenameg = "StructureData/margutti2018g.csv"
     filenameE = "StructureData/margutti2018E.csv"
     thgp, gp = np.loadtxt(filenameg, skiprows=2, unpack=True, delimiter=',')
@@ -233,6 +253,8 @@ def margutti2018(theta):
 
 def mizuta2009(theta, E0=1.0e52):
 
+    # data is dE/dOmega, scaled to 1 at theta=0
+
     filename = "StructureData/mizuta2009E.csv"
     thp, Ep = np.loadtxt(filename, skiprows=2, unpack=True, delimiter=',')
 
@@ -248,6 +270,8 @@ def mizuta2009(theta, E0=1.0e52):
 
 def lazzati2017(theta):
 
+    # data is E_iso
+
     filenameg = "StructureData/lazzati2017g.csv"
     filenameE = "StructureData/lazzati2017E.csv"
     thgp, gp = np.loadtxt(filenameg, skiprows=2, unpack=True, delimiter=',')
@@ -256,7 +280,25 @@ def lazzati2017(theta):
     thgp *= np.pi/180.0
     thEp *= np.pi/180.0
 
+    Ep /= 4.0*np.pi
+
     g = np.interp(theta, thgp, gp, right=1.0)
+    E = np.interp(theta, thEp, Ep, right=0.0)
+
+    return g, E
+
+
+def aloy2005(theta):
+
+    # data is E_iso
+
+    filenameE = "StructureData/Aloy05_B01_500ms_data.csv"
+    thEp, Ep = np.loadtxt(filenameE, unpack=True, delimiter=',')
+
+    thEp *= np.pi/180.0
+    Ep /= 4.0*np.pi
+
+    g = np.zeros(theta.shape)
     E = np.interp(theta, thEp, Ep, right=0.0)
 
     return g, E
@@ -270,8 +312,8 @@ if __name__ == "__main__":
     deg = 180.0/np.pi
     rad = np.pi/180.0
 
-    gG, EG = gaussian(theta, thW=17*rad)
-    gPL, EPL = powerlaw(theta, thW=1.0, thC=0.06, b=4.0)
+    gG, EG = gaussian(theta, thC=6*rad, thW=15*rad, E0=3e52)
+    gPL, EPL = powerlaw(theta, thW=60*deg, thC=4*deg, b=4.0, E0=1e52)
     gBF0303, EBF0303 = boostedFireball(theta)
     gBF1003, EBF1003 = boostedFireball(theta, gb=10)
     gBF0310, EBF0310 = boostedFireball(theta, eta0=10)
@@ -286,6 +328,7 @@ if __name__ == "__main__":
     gM18, EM18 = margutti2018(theta)
     gM09, EM09 = mizuta2009(theta)
     gL17, EL17 = lazzati2017(theta)
+    gA05, EA05 = aloy2005(theta)
 
     print("Plotting")
     fig, ax = plt.subplots(2, 1, figsize=(8, 8))
@@ -394,8 +437,6 @@ if __name__ == "__main__":
              color=clr[4], ls=ls[0])
     axE.plot(theta*deg, EM07p2, label="Morsony 2007 powg2",
              color=clr[4], ls=ls[1])
-    axE.plot(theta*deg, EK17, label="Kathirgamaraju 2017",
-             color=clr[5], ls=ls[0])
     axE.plot(theta*deg, EM18, label="Margutti 2018",
              color=clr[6], ls=ls[0])
     axE.plot(theta*deg, EM09, label="Mizuta 2009",
@@ -414,5 +455,111 @@ if __name__ == "__main__":
     print("Saving structCompE.pdf")
     figE.savefig("structCompE.pdf")
 
-    print("Showing")
-    plt.show()
+    print("Plotting")
+    figE2, axE2 = plt.subplots(1, 1, figsize=(6, 4.5))
+
+    gG, EG = gaussian(theta, thC=6*rad, thW=13*rad, E0=3e52)
+    gPL, EPL = powerlaw(theta, thW=60*rad, thC=4*rad, b=4.5, E0=1e52)
+
+    clr = ["k", "grey", "tab:blue", "tab:orange", "tab:green", "tab:red",
+                "tab:purple", "tab:brown", "tab:pink", "tab:olive", "tab:cyan"]
+    ls = ['-', '--', ':', '-.']
+
+    gaussNorm = 3.0
+    plNorm = 1.0
+
+    axE2.plot(theta*deg, EG*gaussNorm/EG[0],
+              # label=r"Gaussian $\theta_C=6^\circ$ $\theta_W=12^\circ$",
+              label=r"Gaussian",
+              lw=4.0, color=clr[1], ls=ls[0], alpha=0.65)
+    axE2.plot(theta*deg, EPL*plNorm/EPL[0],
+              # label=r"Power law $\theta_C=4^\circ$ $b=4.5$",
+              label=r"Power law",
+              lw=4.0, color=clr[1], ls=ls[1], alpha=0.65)
+    axE2.plot(theta*deg, EA05*gaussNorm/EA05[0], label="Aloy 2005",
+              color=clr[2], ls=ls[0])
+    axE2.plot(theta*deg, EM09*plNorm/EM09[0], label="Mizuta 2009",
+              color=clr[3], ls=ls[0])
+    axE2.plot(theta*deg, EBF1003*plNorm/EBF1003[0],
+              # label=r"Duffell 2013 $\gamma_B=10$ $\eta_0=3$",
+              label=r"Duffell 2013",
+              color=clr[4], ls=ls[0])
+    axE2.plot(theta*deg, EL17*plNorm/EL17[0], label="Lazzati 2017",
+              color=clr[5], ls=ls[0])
+    axE2.plot(theta*deg, EM18*gaussNorm/EM18[0], label="Margutti 2018",
+              color=clr[6], ls=ls[0])
+    # axE2.plot(theta*deg, ED15o, label="Duffell 2015 Oblate",
+    #           color=clr[3], ls=ls[0])
+    # axE2.plot(theta*deg, EM07r5, label="Morsony 2007 realg5",
+    #           color=clr[4], ls=ls[0])
+    # axE2.plot(theta*deg, EM07p2, label="Morsony 2007 powg2",
+    #           color=clr[4], ls=ls[1])
+
+    axE2.legend(loc='upper right', fontsize=13)
+
+    axE2.set_xlabel(r'$\theta$ (degrees)', fontsize=18)
+    axE2.set_ylabel(r'$dE/d\Omega$ (arbitrary units)', fontsize=18)
+    axE2.set_yscale('log')
+    axE2.set_ylim(1.0e-3, 1.0e1)
+    axE2.set_xlim(0.0, 30.0)
+    axE2.tick_params(labelsize=16)
+    figE2.tight_layout()
+
+    print("Saving structCompE2.pdf")
+    figE2.savefig("structCompE2.pdf")
+
+    print("Plotting")
+    figEc, axEc = plt.subplots(1, 1, figsize=(3.5, 3.0))
+
+    gG, EG = gaussian(theta, thC=6*rad, thW=12*rad, E0=3e52)
+    gPL, EPL = powerlaw(theta, thW=60*rad, thC=4*rad, b=4.5, E0=1e52)
+
+    clr = ["k", "grey", "tab:blue", "tab:orange", "tab:green", "tab:red",
+                "tab:purple", "tab:brown", "tab:pink", "tab:olive", "tab:cyan"]
+    ls = ['-', '--', ':', '-.']
+
+    gaussNorm = 3.0
+    plNorm = 1.0
+
+    axEc.plot(theta*deg, EG*gaussNorm/EG[0],
+              # label=r"Gaussian $\theta_C=6^\circ$ $\theta_W=12^\circ$",
+              label=r"Gaussian",
+              lw=2.5, color=clr[1], ls=ls[0], alpha=0.65)
+    axEc.plot(theta*deg, EPL*plNorm/EPL[0],
+              # label=r"Power law $\theta_C=4^\circ$ $b=4.5$",
+              label=r"Power Law",
+              lw=2.5, color=clr[1], ls=ls[1], alpha=0.8)
+    axEc.plot(theta*deg, EA05*gaussNorm/EA05[0], label="Aloy 2005",
+              color=clr[2], ls=ls[0])
+    axEc.plot(theta*deg, EM09*plNorm/EM09[0], label="Mizuta 2009",
+              color=clr[3], ls=ls[0])
+    axEc.plot(theta*deg, EBF1003*plNorm/EBF1003[0],
+              # label=r"Duffell 2013 $\gamma_B=10$ $\eta_0=3$",
+              label=r"Duffell 2013",
+              color=clr[4], ls=ls[0])
+    axEc.plot(theta*deg, EL17*plNorm/EL17[0], label="Lazzati 2017",
+              color=clr[5], ls=ls[0])
+    axEc.plot(theta*deg, EM18*gaussNorm/EM18[0], label="Margutti 2018",
+              color=clr[6], ls=ls[0])
+    # axEc.plot(theta*deg, ED15o, label="Duffell 2015 Oblate",
+    #           color=clr[3], ls=ls[0])
+    # axEc.plot(theta*deg, EM07r5, label="Morsony 2007 realg5",
+    #           color=clr[4], ls=ls[0])
+    # axEc.plot(theta*deg, EM07p2, label="Morsony 2007 powg2",
+    #           color=clr[4], ls=ls[1])
+
+    axEc.legend(loc='upper right', fontsize=8)
+
+    axEc.set_xlabel(r'$\theta$ (degrees)')
+    axEc.set_ylabel(r'$E_{{\rm{iso}}}$ (arbitrary units)')
+    axEc.set_yscale('log')
+    axEc.set_ylim(1.0e-3, 1.0e1)
+    axEc.set_xlim(0.0, 30.0)
+    # axEc.tick_params(labelsize=16)
+    figEc.tight_layout()
+
+    print("Saving structCompEc.pdf")
+    figEc.savefig("structCompEc.pdf")
+
+    # print("Showing")
+    # plt.show()
