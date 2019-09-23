27427921895b1a0e558d74b4a13c73b6e314c1d2
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2019-09-23 14:35:22 -0400

    updates tb fits

--- code/analyzeJetBreaks.py
+++ code/analyzeJetBreaks.py
@@ -2,6 +2,7 @@ import sys
 import math
 import h5py as h5
 import numpy as np
+import scipy.optimize as opt
 import matplotlib.pyplot as plt
 import afterglowpy as grb
 
@@ -30,6 +31,51 @@ def loadData(filename):
     return tN, thV, thC, tj_th, tj_g, tj_pl2, tj_pl6
 
 
+def f_chi2(c, taus38, thC, thV):
+    chi = c[0]*thC + c[1]*thV
+
+    diff = (taus38-chi)/taus38
+    diff2 = (diff*diff).sum()
+    return diff2
+
+
+def fitOffCoeffs(tauses, thCs, thVs):
+
+    Nt = len(tauses)
+    Nc = len(thCs)
+    Nv = len(thVs)
+
+    atau = np.empty((Nt, Nc*Nv))
+    athC = np.empty((Nt, Nc*Nv))
+    athV = np.empty((Nt, Nc*Nv))
+
+    thCgrid, thVgrid = np.meshgrid(thCs, thVs, indexing='ij')
+
+    for i, taus in enumerate(tauses):
+        atau[i, :] = taus.flatten()
+        athC[i, :] = thCgrid.flatten()
+        athV[i, :] = thVgrid.flatten()
+
+    tau = atau.flatten()
+    thC = athC.flatten()
+    thV = athV.flatten()
+
+    offCoeff = 1.0
+
+    off = thV > offCoeff*thC
+    tau38 = np.power(tau, 3.0/8.0)
+
+    guess = (0.65, 0.5)
+
+    res = opt.minimize(f_chi2, guess, (tau38[off], thC[off], thV[off]),
+                       method='TNC', options={'maxiter': 8000})
+
+    if not res.success:
+        print("Fit failed")
+
+    return res.x
+
+
 if __name__ == "__main__":
 
     tN, thVs, thCs, tj_th, tj_g, tj_pl2, tj_pl6 = loadData(sys.argv[1])
@@ -38,15 +84,43 @@ if __name__ == "__main__":
     tau_pl2 = tj_pl2 / tN
     tau_pl6 = tj_pl6 / tN
 
-    thCpVs = thCs[:, None] + thVs[None, :]
-    x = np.linspace(thCpVs.min(), thCpVs.max(), 100)
-
     num = math.pow(0.15, 3.0/8.0)
 
-    chiOnCoeff = 1.18
-    chiOffCoeff = 0.53
+    cCg, cVg = fitOffCoeffs([tau_g], thCs, thVs)
+    print("Fit - Off - Gaussian:  cC = {0:.6f} cV = {1:.6f}"
+          .format(cCg, cVg))
+    cCpl2, cVpl2 = fitOffCoeffs([tau_pl2], thCs, thVs)
+    print("Fit - Off - Powerlaw2: cC = {0:.6f} cV = {1:.6f}"
+          .format(cCpl2, cVpl2))
+    cCpl6, cVpl6 = fitOffCoeffs([tau_pl6], thCs, thVs)
+    print("Fit - Off - Powerlaw6: cC = {0:.6f} cV = {1:.6f}"
+          .format(cCpl6, cVpl6))
+    cCg6, cVg6 = fitOffCoeffs([tau_g, tau_pl6], thCs, thVs)
+    print("Fit - Off - PL6 & Gau: cC = {0:.6f} cV = {1:.6f}"
+          .format(cCg6, cVg6))
+    cCa, cVa = fitOffCoeffs([tau_g, tau_pl2, tau_pl6], thCs, thVs)
+    print("Fit - Off - All:       cC = {0:.6f} cV = {1:.6f}"
+          .format(cCa, cVa))
 
-    chi = np.maximum(1.18*thCs[:, None], 0.53*(thCs[:, None] + thVs[None, :]))
+    cOn = math.pow(((tau_g[:, 0]+tau_pl2[:, 0]+tau_pl6[:, 0])
+                   * np.power(thCs, -8.0/3.0)).mean() / 3.0,
+                   3.0/8.0)
+    print("On-Axis - All: cOn = {0:.6f}".format(cOn))
+
+    cC = cCa  # 0.65
+    cV = cVa  # 0.5
+
+    print("On-Axis t_coeff:  {0:.6f}".format(math.pow(cOn, 8.0/3.0)))
+    print("Off-Axis t_coeff: {0:.6f}".format(math.pow(cV, 8.0/3.0)))
+
+    print("Off-axis term: theta_obs + {0:.6f} * theta_c".format(cC/cV))
+    print("On/Off cutoff: theta_obs / theta_c = {0:.6f}".format(
+          (cOn - cC) / cV))
+
+    chiOff = cC*thCs[:, None] + cV*thVs[None, :]
+
+    x = np.linspace(chiOff.min(), chiOff.max(), 100)
+    chi = np.maximum(cOn*thCs[:, None], chiOff)
     color = ['C{0:d}'.format(i) for i in range(10)]
 
     E53 = 1.0e53
@@ -56,40 +130,49 @@ if __name__ == "__main__":
     th0p1 = 0.1
     th0p2 = 0.2
     th0p3 = 0.3
+    th0p5 = 0.5
 
     coeff_On_53_00_0p1_s = math.pow(9*E53/(16*math.pi*grb.mp*n00*grb.c**5),
-                                    1.0/3.0) * math.pow(chiOnCoeff * th0p1,
+                                    1.0/3.0) * math.pow(cOn * th0p1,
                                                         8.0/3.0)
     coeff_On_53_00_0p1_d = coeff_On_53_00_0p1_s * grb.sec2day
     coeff_Off_53_00_0p1_s = math.pow(9*E53/(16*math.pi*grb.mp*n00*grb.c**5),
-                                     1.0/3.0) * math.pow(chiOffCoeff * th0p1,
+                                     1.0/3.0) * math.pow(cV * th0p1,
                                                          8.0/3.0)
     coeff_Off_53_00_0p1_d = coeff_Off_53_00_0p1_s * grb.sec2day
     coeff_Off_53_00_0p2_s = math.pow(9*E53/(16*math.pi*grb.mp*n00*grb.c**5),
-                                     1.0/3.0) * math.pow(chiOffCoeff * th0p2,
+                                     1.0/3.0) * math.pow(cV * th0p2,
                                                          8.0/3.0)
     coeff_Off_53_00_0p2_d = coeff_Off_53_00_0p2_s * grb.sec2day
     coeff_Off_53_00_0p3_s = math.pow(9*E53/(16*math.pi*grb.mp*n00*grb.c**5),
-                                     1.0/3.0) * math.pow(chiOffCoeff * th0p3,
+                                     1.0/3.0) * math.pow(cV * th0p3,
                                                          8.0/3.0)
     coeff_Off_53_00_0p3_d = coeff_Off_53_00_0p3_s * grb.sec2day
+    coeff_Off_53_00_0p5_s = math.pow(9*E53/(16*math.pi*grb.mp*n00*grb.c**5),
+                                     1.0/3.0) * math.pow(cV * th0p5,
+                                                         8.0/3.0)
+    coeff_Off_53_00_0p5_d = coeff_Off_53_00_0p5_s * grb.sec2day
 
     coeff_On_50_m3_0p1_s = math.pow(9*E50/(16*math.pi*grb.mp*nm3*grb.c**5),
-                                    1.0/3.0) * math.pow(chiOnCoeff * th0p1,
+                                    1.0/3.0) * math.pow(cOn * th0p1,
                                                         8.0/3.0)
     coeff_On_50_m3_0p1_d = coeff_On_50_m3_0p1_s * grb.sec2day
     coeff_Off_50_m3_0p1_s = math.pow(9*E50/(16*math.pi*grb.mp*nm3*grb.c**5),
-                                     1.0/3.0) * math.pow(chiOffCoeff * th0p1,
+                                     1.0/3.0) * math.pow(cV * th0p1,
                                                          8.0/3.0)
     coeff_Off_50_m3_0p1_d = coeff_Off_50_m3_0p1_s * grb.sec2day
     coeff_Off_50_m3_0p2_s = math.pow(9*E50/(16*math.pi*grb.mp*nm3*grb.c**5),
-                                     1.0/3.0) * math.pow(chiOffCoeff * th0p2,
+                                     1.0/3.0) * math.pow(cV * th0p2,
                                                          8.0/3.0)
     coeff_Off_50_m3_0p2_d = coeff_Off_50_m3_0p2_s * grb.sec2day
     coeff_Off_50_m3_0p3_s = math.pow(9*E50/(16*math.pi*grb.mp*nm3*grb.c**5),
-                                     1.0/3.0) * math.pow(chiOffCoeff * th0p3,
+                                     1.0/3.0) * math.pow(cV * th0p3,
                                                          8.0/3.0)
     coeff_Off_50_m3_0p3_d = coeff_Off_50_m3_0p3_s * grb.sec2day
+    coeff_Off_50_m3_0p5_s = math.pow(9*E50/(16*math.pi*grb.mp*nm3*grb.c**5),
+                                     1.0/3.0) * math.pow(cV * th0p5,
+                                                         8.0/3.0)
+    coeff_Off_50_m3_0p5_d = coeff_Off_50_m3_0p5_s * grb.sec2day
 
     print("tj_On_E53_n00_th0p1 = {0:.4e} s = {1:.4e} days".format(
           coeff_On_53_00_0p1_s, coeff_On_53_00_0p1_d))
@@ -99,6 +182,8 @@ if __name__ == "__main__":
           coeff_Off_53_00_0p2_s, coeff_Off_53_00_0p2_d))
     print("tj_Off_E53_n00_th0p3 = {0:.4e} s = {1:.4e} days".format(
           coeff_Off_53_00_0p3_s, coeff_Off_53_00_0p3_d))
+    print("tj_Off_E53_n00_th0p5 = {0:.4e} s = {1:.4e} days".format(
+          coeff_Off_53_00_0p5_s, coeff_Off_53_00_0p5_d))
     print("tj_On_E50_nm3_th0p1 = {0:.4e} s = {1:.4e} days".format(
           coeff_On_50_m3_0p1_s, coeff_On_50_m3_0p1_d))
     print("tj_Off_E50_nm3_th0p1 = {0:.4e} s = {1:.4e} days".format(
@@ -107,6 +192,8 @@ if __name__ == "__main__":
           coeff_Off_50_m3_0p2_s, coeff_Off_50_m3_0p2_d))
     print("tj_Off_E50_nm3_th0p3 = {0:.4e} s = {1:.4e} days".format(
           coeff_Off_50_m3_0p3_s, coeff_Off_50_m3_0p3_d))
+    print("tj_Off_E50_nm3_th0p5 = {0:.4e} s = {1:.4e} days".format(
+          coeff_Off_50_m3_0p5_s, coeff_Off_50_m3_0p5_d))
 
     fig, ax = plt.subplots(3, 2, figsize=(8, 9))
     ax[0, 0].plot(thCs, thCs, color='grey', ls='--')
@@ -116,18 +203,36 @@ if __name__ == "__main__":
     for j in range(len(thVs)):
         ax[0, 0].plot(thCs, chi[:, j], alpha=0.5, color=color[j % 10])
         ax[0, 0].plot(thCs, np.power(tau_g[:, j], 0.375))
-        ax[0, 1].plot(thCs, chi[:, j] / (thCs+thVs[j]),
+        ax[0, 1].plot(thCs, chi[:, j] / chiOff[:, j],
                       alpha=0.5, color=color[j % 10])
-        ax[0, 1].plot(thCs, np.power(tau_g[:, j], 0.375) / (thCs+thVs[j]))
+        ax[0, 1].plot(thCs, np.power(tau_g[:, j], 0.375) / chiOff[:, j])
     for i in range(len(thCs)):
         ax[1, 0].plot(thVs, chi[i, :],
                       alpha=0.5, color=color[i % 10])
         ax[1, 0].plot(thVs, np.power(tau_g[i, :], 0.375))
-        ax[1, 1].plot(thVs, chi[i, :] / (thCs[i]+thVs),
+        ax[1, 1].plot(thVs, chi[i, :] / chiOff[i, :],
                       alpha=0.5, color=color[i % 10])
-        ax[1, 1].plot(thVs, np.power(tau_g[i, :], 0.375) / (thCs[i]+thVs))
-    ax[2, 0].plot(thCpVs.flat, np.power(tau_g.flat, 0.375), ls='', marker='.')
-    ax[2, 0].plot(x, 0.55*x, ls='-', color='grey')
+        ax[1, 1].plot(thVs, np.power(tau_g[i, :], 0.375) / chiOff[i, :])
+    ax[2, 0].plot(chiOff.flat, np.power(tau_g.flat, 0.375), ls='', marker='.')
+    ax[2, 0].plot(x, x, ls='-', color='grey')
+
+    ax[0, 0].set_xlabel(r'$\theta_{\mathrm{c}}$')
+    ax[0, 0].set_ylabel(r'$(t_{\mathrm{b}} / t_N)^{3/8}$')
+    ax[0, 1].set_xlabel(r'$\theta_{\mathrm{c}}$')
+    ax[0, 1].set_ylabel(r'$(t_{\mathrm{b}} / t_N)^{3/8}$')
+    ax[0, 1].set_ylabel(r'$(t_{\mathrm{b}} / t_N)^{3/8}'
+                        r'/ (c_{\mathrm{c}} \theta_{\mathrm{c}}'
+                        r'+ c_{\mathrm{obs}}\theta_{\mathrm{obs}})$')
+    ax[1, 0].set_xlabel(r'$\theta_{\mathrm{obs}}$')
+    ax[1, 0].set_ylabel(r'$(t_{\mathrm{b}} / t_N)^{3/8}$')
+    ax[1, 1].set_xlabel(r'$\theta_{\mathrm{obs}}$')
+    ax[1, 1].set_ylabel(r'$(t_{\mathrm{b}} / t_N)^{3/8}'
+                        r'/ (c_{\mathrm{c}} \theta_{\mathrm{c}}'
+                        r'+ c_{\mathrm{obs}}\theta_{\mathrm{obs}})$')
+    ax[2, 0].set_xlabel(r'$c_{\mathrm{c}} \theta_{\mathrm{c}}'
+                        r'+ c_{\mathrm{obs}}\theta_{\mathrm{obs}}$')
+    ax[2, 0].set_ylabel(r'$t_{\mathrm{b}} / t_N$')
+    fig.tight_layout()
 
     nz = thVs > 0.0
 
@@ -139,20 +244,20 @@ if __name__ == "__main__":
     for j in range(len(thVs)):
         axll[0, 0].plot(thCs, chi[:, j], alpha=0.5, color=color[j % 10])
         axll[0, 0].plot(thCs, np.power(tau_g[:, j], 0.375))
-        axll[0, 1].plot(thCs, chi[:, j] / (thCs+thVs[j]),
+        axll[0, 1].plot(thCs, chi[:, j] / chiOff[:, j],
                         alpha=0.5, color=color[j % 10])
-        axll[0, 1].plot(thCs, np.power(tau_g[:, j], 0.375) / (thCs+thVs[j]))
+        axll[0, 1].plot(thCs, np.power(tau_g[:, j], 0.375) / chiOff[:, j])
     for i in range(len(thCs)):
         axll[1, 0].plot(thVs[nz], chi[i, :][nz],
                         alpha=0.5, color=color[i % 10])
         axll[1, 0].plot(thVs[nz], np.power(tau_g[i][nz], 0.375))
-        axll[1, 1].plot(thVs[nz], chi[i, :][nz] / (thCs[i]+thVs[nz]),
+        axll[1, 1].plot(thVs[nz], chi[i, :][nz] / chiOff[i, :][nz],
                         alpha=0.5, color=color[i % 10])
         axll[1, 1].plot(thVs[nz],
-                        np.power(tau_g[i][nz], 0.375) / (thCs[i]+thVs[nz]))
-    axll[2, 0].plot(thCpVs.flat, np.power(tau_g.flat, 0.375),
+                        np.power(tau_g[i][nz], 0.375) / chiOff[i, :][nz])
+    axll[2, 0].plot(chiOff.flat, np.power(tau_g.flat, 0.375),
                     ls='', marker='.')
-    axll[2, 0].plot(x, 0.55*x, ls='-', color='grey')
+    axll[2, 0].plot(x, x, ls='-', color='grey')
 
     axll[0, 0].set_xscale('log')
     axll[0, 0].set_yscale('log')
@@ -165,8 +270,22 @@ if __name__ == "__main__":
     axll[1, 1].set_xscale('log')
     axll[1, 1].set_yscale('log')
 
-    print(((tau_g[:, 0]+tau_pl2[:, 0]+tau_pl6[:, 0])
-           * np.power(thCs, -8.0/3.0)).mean() / 3.0)
+    axll[0, 0].set_xlabel(r'$\theta_{\mathrm{c}}$')
+    axll[0, 0].set_ylabel(r'$(t_{\mathrm{b}} / t_N)^{3/8}$')
+    axll[0, 1].set_xlabel(r'$\theta_{\mathrm{c}}$')
+    axll[0, 1].set_ylabel(r'$(t_{\mathrm{b}} / t_N)^{3/8}'
+                          r'/ (c_{\mathrm{c}} \theta_{\mathrm{c}}'
+                          r'+ c_{\mathrm{obs}}\theta_{\mathrm{obs}})$')
+    axll[1, 0].set_xlabel(r'$\theta_{\mathrm{obs}}$')
+    axll[1, 0].set_ylabel(r'$(t_{\mathrm{b}} / t_N)^{3/8}$')
+    axll[1, 1].set_xlabel(r'$\theta_{\mathrm{obs}}$')
+    axll[1, 1].set_ylabel(r'$(t_{\mathrm{b}} / t_N)^{3/8}'
+                          r'/ (c_{\mathrm{c}} \theta_{\mathrm{c}}'
+                          r'+ c_{\mathrm{obs}}\theta_{\mathrm{obs}})$')
+    axll[2, 0].set_xlabel(r'$c_{\mathrm{c}} \theta_{\mathrm{c}}'
+                          r'+ c_{\mathrm{obs}}\theta_{\mathrm{obs}}$')
+    axll[2, 0].set_ylabel(r'$t_{\mathrm{b}} / t_N$')
+    figll.tight_layout()
 
     tau_theo = np.power(chi, 8.0/3.0)
     tb_lim_min = 1.0e-4
